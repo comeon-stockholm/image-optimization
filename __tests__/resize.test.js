3c3e71ce40e406d003ba27d8a517d2f50d13bfc1
Velu Gautam <velu.gautam@comeon.com>
2020-07-10 12:25:06 +0530

    Fixed test case and updated github actions * Fixed one image path for the test case * Removed github actions that running against node 10x since 12x is enough

--- __tests__/resize.test.js
+++ __tests__/resize.test.js
@@ -113,12 +113,12 @@ async function testCacheControlFail() {
 }
 
 const backgroundImageSource = [
-    '__tests__/testdata/in/aloha_mobile_html_sw.png',
-    '/background/Mobile-bla.png',
+    '__tests__/testdata/in/Mobile-luckyzoiac.jpg',
+    '/background/Mobile-bla.jpg',
     '__tests__/testdata/out',
     'mobile-icon',
-    'mobile-icon-aloha',
-    '.png',
+    'Mobile-luckyzoiac',
+    '.jpg',
 ];
 
 async function testbackgroundImageSource() {
